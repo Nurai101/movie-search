520f18da3168279c6911f33c2f8d853bdea75d12
Nurai <nuraitasybek17@gmail.com>
2024-06-10 22:53:28 +0300

    функция поиска фильма

--- script.js
+++ script.js
@@ -13,7 +13,44 @@ fetch(path)
     console.log(data);
   });
 
+function searchMovies(e) {
+  e.preventDefault();
+  single_movieEl.innerHTML = '';
+  const term = search.value;
+  if (term.trim()) {
+    fetch(path)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        resultHeading.innerHTML = `<h2>Результаты поиска для '${term}':</h2>`;
 
+        const filteredMovies = data.filter(movie =>
+          movie.title.toLowerCase().includes(term.toLowerCase())
+        );
+
+        if (filteredMovies.length === 0) {
+          resultHeading.innerHTML = `<p>Нет результатов. Попробуйте снова!</p>`;
+        } else {
+          moviesEl.innerHTML = filteredMovies
+            .map(
+              movie => `
+            <div class="movie">
+              <img src="${movie.image_url}" alt="${movie.title}" />
+              <div class="movie-info" data-movieID="${movie.id}">
+                <h3>${movie.title}</h3>
+                <p>${movie.author}</p>
+              </div>
+            </div>
+          `
+            )
+            .join('');
+        }
+      });
+    search.value = '';
+  } else {
+    alert('Пожалуйста, введите запрос');
+  }
+}
 
 
 
