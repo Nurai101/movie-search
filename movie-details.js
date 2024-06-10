a23ae69cdcc92325884da135fc03b2565a338a74
Nurai <nuraitasybek17@gmail.com>
2024-06-10 22:59:29 +0300

    страница с инфой о фильме

--- movie-details.js
+++ movie-details.js
@@ -1,1 +1,6 @@
+const singleMovieEl = document.getElementById('single-movie');
+const path = "./film.json";
+const urlParams = new URLSearchParams(window.location.search);
+const movieID = urlParams.get('id');
+
 
