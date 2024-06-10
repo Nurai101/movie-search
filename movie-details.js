254045b4595cae64d3dd264d7e68a1f38e3151ac
Nurai <nuraitasybek17@gmail.com>
2024-06-10 23:01:54 +0300

    функция которая выводитинфу о фильме на страницу

--- movie-details.js
+++ movie-details.js
@@ -20,3 +20,19 @@ function getMovieById(movieID) {
     });
 }
 
+function displayMovieDetails(movie) {
+  singleMovieEl.innerHTML = `
+    <div class="single-movie">
+      <h1>${movie.title}</h1>
+      <img src="${movie.image_url}" alt="${movie.title}" />
+      <div class="single-movie-info">
+        <p>Автор: ${movie.author}</p>
+        <p>Жанр: ${movie.genre}</p>
+        <p>Год: ${movie.year}</p>
+      </div>
+      <div class="main">
+        <p>${movie.description}</p>
+      </div>
+    </div>
+  `;
+}
