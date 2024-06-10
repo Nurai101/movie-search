52cb4cf10b5c995a51fb7b774ac5c3cc44bb8266
Nurai <nuraitasybek17@gmail.com>
2024-06-10 22:52:44 +0300

    получение пути к джейсон файлу

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const search = document.getElementById('search'),
+  submit = document.getElementById('submit'),
+  random = document.getElementById('random'),
+  moviesEl = document.getElementById('movies'),
+  resultHeading = document.getElementById('result-heading'),
+  single_movieEl = document.getElementById('single-movie');
+
+const path = "./film.json";
+
+fetch(path)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  });
+
+
+
+
+
+
 
 
 
