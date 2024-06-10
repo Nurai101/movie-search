fd0927753e9e8d1e0fd0dc3f0827792f0e3c465a
Nurai <nuraitasybek17@gmail.com>
2024-06-10 23:01:09 +0300

    функция которая выводит фильм на который кликнули на прошлой странице

--- movie-details.js
+++ movie-details.js
@@ -3,4 +3,20 @@ const path = "./film.json";
 const urlParams = new URLSearchParams(window.location.search);
 const movieID = urlParams.get('id');
 
+function getMovieById(movieID) {
+  fetch(path)
+    .then(res => res.json())
+    .then(data => {
+      const movie = data.find(m => m.id === parseInt(movieID));
+      if (movie) {
+        displayMovieDetails(movie);
+      } else {
+        singleMovieEl.innerHTML = '<h2>Фильм не найден</h2>';
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching movie data:', error);
+      singleMovieEl.innerHTML = '<h2>Ошибка загрузки данных фильма</h2>'; 
+    });
+}
 
